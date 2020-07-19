15169c7c426b133d40e4b16b9af3367f2270a493
RafaelRez75 <rrezende75@gmail.com>
2020-07-19 20:02:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Projeto de gerador de notícias
+
+Segue o passo a passo para execução do projeto:
+
+1- Baixar o Sistema Gerenciador de Bando de Dados MariaDB
+
+O sistema servirá a fim de cadastrar notícias e fazer leitura das mesmas. É recomendável fazer o download pelo link:
+https://downloads.mariadb.org/interstitial/mariadb-10.2.10/winx64-packages/mariadb-10.2.10-winx64.msi/from/http%3A//ftp.hosteurope.de/mirror/archive.mariadb.org/
+
+2 - Acessar o cmd pelo caminho na pasta 'bin' da pasta do MariaDB que estará nos Arquivos de Programa
+
+3 - Digitar o seguinte comando "mysql -u root -p" e definir a senha como root
+
+4 - Criar a base de dados "lab_es" com o comando "create database lab_es"
+
+5 - Abrir outro cmd com o caminho da pasta "simple_web_app_list"
+
+6 - Digitar o comando "gradle apprun"
+
+
+OBS.: É necessário previamente já ter instalado o Gradle senão o comando gradle apprun não surtirá efeito
---
+++ java_orm_jpa_hibernate-master/simple_crud/src/main/java/model/Noticia.java
@@ -0,0 +1,36 @@
+/*
+** This file contains the persistence class.
+** Each class attribute is known as persistence attribute
+ */
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity(name="noticia")
+public class Noticia { //persistence class
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;           //persistence attribute
+    @Column(name="textonoticia")
+    private  String textonoticia;       //persistence attribute
+	
+    public Noticia(){}          //zero parameter constructor
+    
+	public String getNoticia() {   
+        return textonoticia;
+    }
+    public void setNoticia(String textonoticia) {
+        this.textonoticia = textonoticia;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }  
+}
